3ec43fea0b9071b6932bdf23d96cd11b44595fec
CocoGzh <guozhenhao17@mails.ucas.ac.cn>
2026-09-03 21:31:53 +0800

    plotly config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,5 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = ['custom.css']
+nbsphinx_execute = 'never'
+nbsphinx_allow_errors = False
